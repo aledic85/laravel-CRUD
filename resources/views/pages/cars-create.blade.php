93be8b643a9e03f2c5ed9f27ea075dfd0086d5fb
aledic85 <dicuollo85@gmail.com>
2019-05-21 15:58:28 +0200

    add display the error messages in the view

--- resources/views/pages/cars-create.blade.php
+++ resources/views/pages/cars-create.blade.php
@@ -3,6 +3,17 @@
 @section ('content')
 
   <h1>Create new car</h1>
+
+  @if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+  @endif
+
   <form action="{{route('cars.store')}}" method="post">
     @csrf
 
